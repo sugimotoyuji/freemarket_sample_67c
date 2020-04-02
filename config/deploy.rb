52d1ac14d5f26280a7f7ea69530ee1a49b484373
ikedayohei <yoheii0612@gmail.com>
2020-04-02 21:42:30 +0900

    Capistrano install

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.12.1'
+
+# Capistranoのログの表示に利用する
+set :application, 'freemarket_sample_67c'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:sugimotoyuji/freemarket_sample_67c.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+# chat-spaceで使ったpemを指定
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/yohei9021.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+set :linked_files, %w{ config/master.key }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
