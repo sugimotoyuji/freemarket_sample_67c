ed94c85e594f483454f6cadc069bd106f3521831
ikedayohei <yoheii0612@gmail.com>
2020-04-03 13:11:02 +0900

    deploy.rb key  変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,11 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
-
-#Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-
-#ポート番号を指定
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-
-#エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-
-#通常のログを記録するファイルを指定
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 timeout 60
@@ -33,7 +22,7 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 
   if run_once
-    run_once = false 
+    run_once = false # prevent from firing again
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
