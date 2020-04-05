31562522a9e21c39ec580d0a620b8c69f78b250b
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-05 17:54:48 +0900

    Update user.rb
    validationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :first_name, :first_name_reading, :last_name,
+            :last_name_reading, :telephone ,presence: true
+  
 end
