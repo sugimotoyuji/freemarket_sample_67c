13ef8573b1f9f46aa4ae36acc2e4726d10cd6abd
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-05 18:02:08 +0900

    Update user.rb
    記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :first_name, :first_name_reading, :last_name,
-            :last_name_reading, :telephone ,presence: true
+  validates :nickname, :first_name, :first_name_reading, :last_name, :last_name_reading, :telephone ,presence: true
   has_many :items
   has_many :orders,dependent: :destroy
   has_many :profits
@@ -12,9 +11,9 @@ class User < ApplicationRecord
   has_many :communicatiuons,dependent: :destroy
   has_many :likes,dependent: :destroy
   has_many :flags,dependent: :destroy
-  has_many :message_items,through::messages,source::items
-  has_many :like_items,through::likes,source::items
-  has_many :flag_items,through::flags,source::items
+  has_many :message_items, through: :messages,source: :items
+  has_many :like_items, through: :likes,source: :items
+  has_many :flag_items, through: :flags,source: :items
   has_many :addresses
   has_many :todo_lists
   belongs_to :rate
