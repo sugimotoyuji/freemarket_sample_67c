c78d17bfcaa0c5c15f6f7f769d9b5d44b1207b32
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-05 17:55:50 +0900

    Update user.rb
    Associationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, :first_name, :first_name_reading, :last_name,
             :last_name_reading, :telephone ,presence: true
-  
+  has_many :items
+  has_many :orders,dependent: :destroy
+  has_many :profits
+  has_many :points
+  has_many :communicatiuons,dependent: :destroy
+  has_many :likes,dependent: :destroy
+  has_many :flags,dependent: :destroy
+  has_many :message_items,through::messages,source::items
+  has_many :like_items,through::likes,source::items
+  has_many :flag_items,through::flags,source::items
+  has_many :addresses
+  has_many :todo_lists
+  belongs_to :rate
+  belongs_to :payment_information,dependent: :destroy
 end
