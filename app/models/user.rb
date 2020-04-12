8e6248b4cee2d9198e36463a605a99456d15adc5
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-12 22:19:58 +0900

    Update user.rb
    Emailのvalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
