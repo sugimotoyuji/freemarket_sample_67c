ff7f45a863548efd8acd2eabf745315451860755
ikedayohei <yoheii0612@gmail.com>
2020-04-15 16:45:58 +0900

    エラーメッセージ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,10 +23,10 @@ class ItemsController < ApplicationController
 
   def create
    @item = Item.new(item_params)
-   if @item.save!
+   if @item.save
     redirect_to root_path
    else
-    redirect_to root_path
+    render :new
    end
   end
 
