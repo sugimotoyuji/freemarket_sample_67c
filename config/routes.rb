2f1f232c3ac225ca632964564e5b69ebe2a8fef8
kazuyan0825 <kigawa42@gmail.com>
2020-04-16 21:12:14 +0900

    商品購入ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'front#index'
-   resources :items do
-   end
+  resources :items, only: [:index,:new, :show, :create, :edit , :update, :destroy] do
+    collection do
+      get "buy"
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
