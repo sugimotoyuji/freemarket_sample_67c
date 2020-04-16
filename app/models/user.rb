371c899fd5d1d962bad839034a41702e067d5989
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-16 17:39:02 +0900

    Update user.rb
    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 8 }
-  validates :telephone, numericality: { only_integer: true }
 end
