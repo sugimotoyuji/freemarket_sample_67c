fd85882635f182c3a400287d8d157443eb07c4e6
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-16 19:32:45 +0900

    Update 20200416093434_create_addresses.rb

--- db/migrate/20200416093434_create_addresses.rb
+++ db/migrate/20200416093434_create_addresses.rb
@@ -1,7 +1,11 @@
 class CreateAddresses < ActiveRecord::Migration[5.0]
   def change
     create_table :addresses do |t|
-
+      t.integer    :zip_code,  null: false
+      t.string     :city,      null: false
+      t.string     :address,   null: false
+      t.string     :building
+      t.references :user
       t.timestamps
     end
   end
