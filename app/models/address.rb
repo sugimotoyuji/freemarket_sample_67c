40feb0633fa32579d7c6779f8031bdf1e551963b
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-16 19:50:29 +0900

    Update address.rb
    active_hashの記述追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
   validates :zipcode, :city, :address ,presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  
 end
