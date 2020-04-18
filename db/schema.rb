943bbb31e501671b3f1283e596f298b21a5b66a9
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-18 17:15:01 +0900

    データベースロールバック

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200416093434) do
-
-  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "zip_code",   null: false
-    t.integer  "prefecture", null: false
-    t.string   "city",       null: false
-    t.string   "address",    null: false
-    t.string   "building"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
-  end
+ActiveRecord::Schema.define(version: 20200411054718) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                                          null: false
