738dbb252346ff94bc78e4cbdbe8fc97f29178d1
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-18 17:28:42 +0900

    Update 20200416093434_create_addresses.rb
    addressesテーブルのmigrationファイル作成

--- db/migrate/20200416093434_create_addresses.rb
+++ db/migrate/20200416093434_create_addresses.rb
@@ -1,11 +1,16 @@
 class CreateAddresses < ActiveRecord::Migration[5.0]
   def change
     create_table :addresses do |t|
-      t.integer    :zip_code,   null: false
-      t.integer    :prefecture, null: false
-      t.string     :city,       null: false
-      t.string     :address,    null: false
+      t.string     :shipping_first_name,         null: false
+      t.string     :shipping_first_name_reading, null: false
+      t.string     :shipping_last_name,          null: false
+      t.string     :shipping_last_name_reading,  null: false
+      t.integer    :zip_code,                    null: false
+      t.integer    :prefecture,                  null: false
+      t.string     :city,                        null: false
+      t.string     :address,                     null: false
       t.string     :building
+      t.string     :shipping_telephone
       t.references :user
       t.timestamps
     end
