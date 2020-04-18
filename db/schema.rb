84884204adbaec9f364ee7294643b1cff3c940bc
ikedayohei <yoheii0612@gmail.com>
2020-04-18 17:45:46 +0900

    マイグレーションファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20200417140438) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -87,7 +86,7 @@ ActiveRecord::Schema.define(version: 20200417140438) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                                          null: false
     t.string   "first_name",                                        null: false
@@ -113,8 +112,6 @@ ActiveRecord::Schema.define(version: 20200417140438) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  
   add_foreign_key "item_images", "items"
   add_foreign_key "items", "users"
 end
-
