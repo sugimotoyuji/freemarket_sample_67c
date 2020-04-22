d134a4b4ae7dadf4246466785346d23c2a65d0e2
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-22 18:32:23 +0900

    ルーティング、コントローラ設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  resources :users
+  resources :users,only: :show
   root 'front#index'
    resources :items do
     collection do
