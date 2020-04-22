76cecbc73d661ae04c6eec1049c97b1cd417dd4e
ryota828 <ry0ta256@yahoo.co.jp>
2020-04-22 18:15:12 +0900

    Update routes.rb
    ユーザーマイページ用にルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+  resources :users
   root 'front#index'
    resources :items do
     collection do
