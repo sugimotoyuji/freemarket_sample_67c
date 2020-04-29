fa87ce5d78193a0734cc8c3474aae6f7d97f3ee1
ikedayohei <yoheii0612@gmail.com>
2020-04-29 17:53:39 +0900

    SNS add

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,7 +10,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def create
+    if params[:sns_auth] == 'true'
+      pass = Devise.friendly_token
+      params[:user][:password] = pass
+      params[:user][:password_confirmation] = pass
+    else
     @user = User.new(sign_up_params)
+    end
     unless @user.valid?
       flash.now[:alert] = @user.errors.full_messages
       render :new and return
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,6 @@ class User < ApplicationRecord
       sns.user = user
       sns.save
     end
-    user
+    { user: user, sns: sns }
   end
 end
