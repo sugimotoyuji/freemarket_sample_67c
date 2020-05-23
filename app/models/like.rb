8375b9815ae6ebcd811ce0a2d8747f1642c5ae23
kazuyan0825 <kigawa42@gmail.com>
2020-05-23 22:05:23 +0900

    修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,5 @@ class Like < ApplicationRecord
   belongs_to :item, counter_cache: :likes_count
   belongs_to :user
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
   validates_uniqueness_of :item_id, scope: :user_id
 end
