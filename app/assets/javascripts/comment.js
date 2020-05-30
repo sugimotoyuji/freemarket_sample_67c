3b611d062f7515a842f427e85f569755f22bcafe
kazuyan0825 <kigawa42@gmail.com>
2020-05-30 13:50:01 +0900

    コメント非同期

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,17 +1,22 @@
 $(function(){
   function buildHTML(comment){
     var html = `
-                  <div class="commentBox__list__content">
-                  <div class="commentBox__list__content__name">
-                  
+                  <div class="commentText__list">
+                  <div class="commentText__list__icon">
+                  <i class="fa fa-user"></i>
                   </div>
-                  <div class="commentBox__list__content__message">
+                  <div class="commentText__list__content">
+                  <div class="commentText__list__content__name">
+                  ${comment.user_name}
+                  </div>
+                  <div class="commentText__list__content__message">
                   ${comment.text}
                   </div>
-                  <div class="commentBox__list__content__date">
-                 
+                  <div class="commentText__list__content__date">
+                  ${comment.created_at}
                   </div>
-                  </div>`
+                  </div>
+                  `
     return html;
   }
   $('#new_comment').on('submit', function(e){
@@ -28,9 +33,10 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.comments').append(html);
+      
+      $('.commentText').append(html);
       $('.textbox').val('');
-      $('.form__submit').prop('disabled', false);
+      $('.commentBtn').prop('disabled', false);
     })
   })
 })
