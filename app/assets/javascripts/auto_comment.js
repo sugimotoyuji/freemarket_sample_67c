f6b9a11fe9bb1822190f23cb313286cb307243a9
kazuyan0825 <kigawa42@gmail.com>
2020-06-02 20:45:21 +0900

    コメント自動更新修正

--- app/assets/javascripts/auto_comment.js
+++ app/assets/javascripts/auto_comment.js
@@ -33,7 +33,7 @@ $(function(){
           data: {id: last_comment_id}
         })
         .done(function(comments) {
-          console.log(comments)
+         
           if (comments.length !== 0) {
             var insertHTML = '';
             $.each(comments, function(i, comment) {
