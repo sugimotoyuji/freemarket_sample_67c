91674a742914368b6398754160baccf02dd62322
ikedayohei <yoheii0612@gmail.com>
2020-06-02 22:06:00 +0900

    test code add

---
+++ spec/factories/item.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+
+  factory :item do
+    name {"a"}
+    user_id {"1"}
+    description {"a"}
+    category_id {"1"}
+    size_id {"1"}
+    condition_id {"1"}
+    delivery_charge_id {"1"}
+    delivery_way_id {"1"}
+    delivery_date_id {"1"}
+    price {"300"}
+    brand_id {"1"}
+    order_status_id {"1"}
+  end
+
+end
