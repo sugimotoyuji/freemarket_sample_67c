633cc8991f813964c53097575ac9cb31a9d15764
ryota828 <ry0ta256@yahoo.co.jp>
2020-06-07 23:15:53 +0900

    Update address_controller.rb

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,11 +1,10 @@
 class AddressController < ApplicationController
+  before_action :set_address, only: [:edit,:update]
 
   def edit
-    @address = Address.find(current_user.id)
   end
 
   def update
-    @address = Address.find(current_user.id)
     if @address.update(address_params)
       redirect_to user_path(current_user.id)
     else
@@ -17,6 +16,10 @@ class AddressController < ApplicationController
 
   def address_params
     params.require(:address).permit(:shipping_first_name, :shipping_first_name_reading, :shipping_last_name, :shipping_last_name_reading, :zip_code, :prefecture, :city, :address, :building, :shipping_telephone)
+  end
+
+  def set_address
+    @address = Address.find(current_user.id)
   end
 
 end
